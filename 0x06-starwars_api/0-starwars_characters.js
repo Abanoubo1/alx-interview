ac70470037411940e24490e5f48043a94f185378
Abanoubo1 <abanoubwageh88@gmail.com>
2024-04-19 03:35:49 +0200

    request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,45 +2,30 @@
 
 const request = require('request');
 
-// Check if a movie ID was provided
-if (process.argv.length < 3) {
-  console.log('Please provide a movie ID as an argument.');
-  process.exit(1);
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-const movieId = process.argv[2]; // Get the movie ID from the command-line argument
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`; // Construct the URL for the movie
-
-request(url, { json: true }, (error, response, body) => {
+request(movieEndpoint, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode);
-    return;
-  }
-
-  if (body.characters && body.characters.length > 0) {
-    body.characters.forEach(characterUrl => {
-      // Extract the character ID from the URL
-      const characterId = characterUrl.match(/\/api\/people\/(\d+)\//)[1];
-      request(`https://swapi.dev/api/people/${characterId}/`, { json: true }, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-          return;
-        }
-
-        if (response.statusCode !== 200) {
-          console.error('Error:', response.statusCode);
-          return;
-        }
-
-        console.log(body.name); // Print the character's name
-      });
-    });
+    console.log(error);
   } else {
-    console.log('No characters found for this movie.');
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
   }
 });

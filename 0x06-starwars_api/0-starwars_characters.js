9b902fbe0fe1474cbf5c38038b163f6942c35cf2
Abanoubo1 <abanoubwageh88@gmail.com>
2024-04-19 04:01:58 +0200

    try

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,49 @@
-#!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
+import requests
+import sys
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+def get_movie_characters(movie_id):
+    # Define the base URL of the Star Wars API
+    base_url = 'https://swapi.dev/api/films/'
+    
+    # Construct the URL for the given movie ID
+    url = f"{base_url}{movie_id}/"
+    
+    try:
+        # Send a GET request to the API to fetch the movie data
+        response = requests.get(url)
+        
+        # Raise an exception if the request was not successful
+        response.raise_for_status()
+        
+        # Parse the JSON data from the response
+        data = response.json()
+        
+        # Get the list of character URLs from the movie data
+        character_urls = data.get('characters', [])
+        
+        # Iterate through each character URL and fetch the character data
+        for character_url in character_urls:
+            char_response = requests.get(character_url)
+            char_response.raise_for_status()
+            char_data = char_response.json()
+            
+            # Print the character's name
+            print(char_data['name'])
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
-}
+def main():
+    # Check if a movie ID was provided as an argument
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <movie_id>")
+        sys.exit(1)
+    
+    # Get the movie ID from the first positional argument
+    movie_id = sys.argv[1]
+    
+    # Fetch and print the characters for the given movie ID
+    get_movie_characters(movie_id)
 
-starwarsCharacters(filmID);
+if __name__ == '__main__':
+    main()

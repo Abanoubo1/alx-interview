3c6e93bf2ec7fd11df605cf71a7998535b789150
Abanoubo1 <abanoubwageh88@gmail.com>
2024-04-19 03:21:16 +0200

    node

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,24 @@
 #!/usr/bin/node
+const req = require('request');
+const url = 'https://swapi-api.alx-tools.com/api';
 
-const request = require('request');
-
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+if (process.argv.length > 2) {
+  const link = `${url}/films/${process.argv[2]}/`;
+  req(link, (error, request, body) => {
+    if (error) {
+      console.log(error);
+    }
+    const name = JSON.parse(body).characters.map(
+      url_ => new Promise((resolve, reject) => {
+        req(url_, (err, reqs, bodi) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(JSON.parse(bodi).name);
+        });
+      }));
+    Promise.all(name)
+      .then(names => console.log(names.join('\n')))
+      .catch(errors => console.log(errors));
   });
-};
+}

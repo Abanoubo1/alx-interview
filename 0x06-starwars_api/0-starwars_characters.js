d373602a324b0f8d717f8b013b32a55236ce520b
Abanoubo1 <abanoubwageh88@gmail.com>
2024-04-19 03:22:17 +0200

    new

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,22 @@
 #!/usr/bin/node
-const req = require('request');
-const url = 'https://swapi-api.alx-tools.com/api';
+
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  const link = `${url}/films/${process.argv[2]}/`;
-  req(link, (error, request, body) => {
-    if (error) {
-      console.log(error);
+  request(`${url}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-    const name = JSON.parse(body).characters.map(
-      url_ => new Promise((resolve, reject) => {
-        req(url_, (err, reqs, bodi) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(JSON.parse(bodi).name);
-        });
-      }));
-    Promise.all(name)
-      .then(names => console.log(names.join('\n')))
-      .catch(errors => console.log(errors));
+    const charactersURL = JSON.parse(body).characters;
+    charactersURL.forEach((characterURL) => {
+      request(characterURL, (err, _, body) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(JSON.parse(body).name);
+      });
+    });
   });
 }
